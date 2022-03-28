5b9fc417927dcd360a7b801fe5e0db226b005ede
Edward Stange <ed@pop-os.localdomain>
2022-03-28 17:28:08 -0600

    updates now on web UI

--- app.js
+++ app.js
@@ -53,6 +53,24 @@ function postData() {
 
 }
 
+// updating (will figure this out later)
+function updatePost(currentUsername, currentTitle, currentDescription, newUsername, newTitle, newDescription) {
+  const data = { 
+      username: username,
+      title: title,
+      text: description,
+      date: date
+    };
+    fetch("https://mynotes33.azurewebsites.net/api/Blog?code=vuwV9RVS2pieuavif8Pc6PLV0ubWg7zSYVjtiRE2sOOHVPh3E/RPdw==", {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
+}
+
 const renderBlogPost = (blogPostInfo) => {
 
   const blogTable = document.getElementById("container");
@@ -81,6 +99,7 @@ const createPostElement = (username, title, text, date) => {
   const blogUser = document.createElement("p")
   const blogDate = document.createElement("p")
   const blogText = document.createElement("p");
+  const updateBtn = document.createElement("button");
   const readMore = document.createElement("button");
 
   blogTable.appendChild(blogPost);
@@ -89,13 +108,18 @@ const createPostElement = (username, title, text, date) => {
   blogPost.appendChild(blogUser);
   blogPost.appendChild(blogDate);
   blogPost.appendChild(blogText);
+  blogPost.appendChild(updateBtn);
   blogPost.appendChild(readMore);
 
   blogTitle.innerText = title
   blogUser.innerText = "By " + username;;
   blogDate.innerText = date;;
   blogText.innerText = text;
+  updateBtn.innerText = "Edit Post";
   readMore.innerText = "Read More";
+  updateBtn.onclick = function() {
+    showEditPost(blogTitle, blogUser, blogText, blogImage, blogPost, updateBtn, readMore, blogDate);
+  }
   readMore.onclick = function() {
     window.location.href='singlePost.html'
   }
@@ -109,3 +133,75 @@ const createPostElement = (username, title, text, date) => {
   blogImage.style.width = "400px";
 }
 
+function showEditPost(blogTitle, blogUser, blogText, blogImage, blogPost, updateBtn, readMore, blogDate) {
+  //Creating DOM elements for the user to use to edit posts
+  let editImage = document.createElement("input");
+  let editTitle = document.createElement("input");
+  let editUser = document.createElement("input");
+  let editText = document.createElement("textarea");
+  const CANCEL_BTN = document.createElement("button");
+  const SAVE_BTN = document.createElement("button");
+
+  //Modifying editable DOM elements to start off containing the original post's content
+  editImage.setAttribute("type", "file");
+  editTitle.setAttribute("type", "text");
+  editUser.setAttribute("type", "text");
+  editText.setAttribute("type", "text");
+  editImage.setAttribute("value", blogImage.src);
+  editTitle.setAttribute("value", blogTitle.innerText);
+  editUser.setAttribute("value", blogUser.innerText);
+  editText.innerText = blogText.innerText;
+  CANCEL_BTN.innerText = "Cancel";
+  SAVE_BTN.innerText = "Save";
+
+  //Putting all DOM elements into arrays to simplify code for next steps
+  let originalItems = [blogImage, blogTitle, blogUser, blogDate, blogText, updateBtn, readMore];
+  let newItems = [editImage, editTitle, editUser, blogDate, editText, CANCEL_BTN, SAVE_BTN];
+
+  //Removing the original content from the post
+  originalItems.forEach((blogItem) => {
+    removeItemFromDOM(blogPost, blogItem);
+  })
+
+  //Adding editable fields for the user
+  newItems.forEach((blogItem) => {
+    addItemToDOM(blogPost, blogItem);
+  })
+
+  //Adding an onclick listener for the cancel button
+  CANCEL_BTN.onclick = function() {
+    putContentBack();
+  }
+
+  //Adding an onclick listener for the save button
+  SAVE_BTN.onclick = function() {
+    // updatePost(blogUser.innerText, blogTitle.innerText, blogText.innerText, editUser.value, editTitle.value, editText.value);
+    blogTitle.innerText = editTitle.value;
+    blogUser.innerText = editUser.value;
+    blogText.innerText = editText.value;
+    blogImage.setAttribute("src", editImage.value);
+
+    putContentBack();
+  }
+
+  //Puts all the content back the way it was before either modified or not so that the post looks normal again
+  function putContentBack() {
+    newItems.forEach((blogItem) => {
+      removeItemFromDOM(blogPost, blogItem);
+    })
+
+    originalItems.forEach((blogItem) => {
+      addItemToDOM(blogPost, blogItem);
+    })
+  }
+}
+
+//Removes a DOM item from a parent DOM element. Both need to be passed in.
+function removeItemFromDOM(parentObject, obectToRemove) {
+  parentObject.removeChild(obectToRemove);
+}
+
+//Adds a DOM item to a parent DOM element. Both need to be passed in.
+function addItemToDOM(parentObject, obectToAdd) {
+  parentObject.appendChild(obectToAdd);
+}
