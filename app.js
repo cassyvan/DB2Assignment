5211c0c5c161c9f8c334ee17c5a4ca04fc7cfa0d
cassyvan <cvan063@mtroyal.ca>
2022-03-29 17:48:26 -0600

    delete post function added

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// let blogData = [];
+let blogPostList = [];
+let url =
+  "https://mynotes33.azurewebsites.net/api/Blog?code=vuwV9RVS2pieuavif8Pc6PLV0ubWg7zSYVjtiRE2sOOHVPh3E/RPdw==";
 
 window.onload = function(){
   getData();
@@ -11,23 +13,23 @@ const alertTest = () => {
 }
 // getting
 function getData() {
-  let url = "https://mynotes33.azurewebsites.net/api/Blog?code=vuwV9RVS2pieuavif8Pc6PLV0ubWg7zSYVjtiRE2sOOHVPh3E/RPdw==";
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      renderBlogPost(data.response);
+      setInitialBlogPosts(data.response);
     })
     .catch((error) => console.log(error));
 }
 
 // posting
 function postData() {
-  var username = document.getElementById("userInput").value;
-  var title = document.getElementById("userTitle").value;
-  var description = document.getElementById("userDescription").value;
+  let username = document.getElementById("userInput").value;
+  let title = document.getElementById("userTitle").value;
+  let description = document.getElementById("userDescription").value;
 
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+ ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  // var today = new Date();
+  // var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+ ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let date = new Date().toLocaleString('en-US')
 
 
   const data = { 
@@ -36,7 +38,7 @@ function postData() {
       text: description,
       date: date
     };
-    fetch('https://mynotes33.azurewebsites.net/api/Blog?code=vuwV9RVS2pieuavif8Pc6PLV0ubWg7zSYVjtiRE2sOOHVPh3E/RPdw==', {
+    fetch(url, {
       method: 'POST', // or 'PUT'
       headers: {
         "Content-Type": "application/json",
@@ -44,49 +46,60 @@ function postData() {
       body: JSON.stringify(data),
     }
   );
-  createPostElement(username, title, description, date);
+  updateBlogPosts(true, data);
   $('form')[0].reset();
 
 }
 
-const renderBlogPost = (blogPostInfo) => {
-  let totalPost = blogPostInfo.length;
-  if (totalPost != 0) {
-    for (let i=0; i < totalPost; i++) {
+const setInitialBlogPosts = (blogPostInfo) => {
+  blogPostInfo.forEach((post) => {
+    blogPostList.push(post);
+  });
 
-      let username = blogPostInfo[i].username
-      let title = blogPostInfo[i].title
-      let text = blogPostInfo[i].text
-      let date = blogPostInfo[i].date
-
-      createPostElement(username, title, text, date);
-    }
-  }
-  console.log(blogPostInfo);
+  renderBlogPosts();
 };
 
-const createPostElement = (username, title, text, date) => {
+const renderBlogPosts = () => {
+  blogPostList.forEach((post) => {
+    createPostElement(post);
+  });
+};
+
+const createPostElement = (post) => {
   const blogTable = document.getElementById("container");
   const blogPost = document.createElement("div");
-  const blogImage = document.createElement("img")
-  const blogTitle = document.createElement("h4")
-  const blogInfo = document.createElement("p")
+  const blogImage = document.createElement("img");
+  const blogTitle = document.createElement("h4");
+  const blogUser = document.createElement("p");
+  const blogDate = document.createElement("p");
+  const blogText = document.createElement("p");
   const readMore = document.createElement("button");
+  const deletePost = document.createElement("button");
 
   blogTable.appendChild(blogPost);
   blogPost.appendChild(blogImage);
   blogPost.appendChild(blogTitle);
-  blogPost.appendChild(blogInfo);
+  blogPost.appendChild(blogUser);
+  blogPost.appendChild(blogDate);
+  blogPost.appendChild(blogText);
   blogPost.appendChild(readMore);
+  blogPost.appendChild(deletePost);
 
-  blogTitle.innerText = title
-  blogInfo.innerText = `By ${username} \n ${date} \n ${text}`
+  blogTitle.innerText = post.title;
+  blogUser.innerText = "By " + post.username;
+  blogDate.innerText = post.date;
+  blogText.innerText = post.text;
   readMore.innerText = "Read More";
-  readMore.onclick = function() {
-    window.location.href=`singlePost.html?username=${username}&title=${title}`
-  }
+  deletePost.innerText = "Delete Post";
 
-  blogPost.id = 
+  readMore.onclick = function () {
+    window.location.href = "singlePost.html";
+  };
+
+  deletePost.onclick = () => {
+    console.log(post._id);
+    updateBlogPosts(false, post);
+  };
   blogPost.setAttribute('align', 'center');
   blogImage.setAttribute('src', '/images/sample.jpg');
 
@@ -95,6 +108,25 @@ const createPostElement = (username, title, text, date) => {
   blogImage.style.height = "400px";
   blogImage.style.width = "400px";
 }
+
+const updateBlogPosts = (add, post) => {
+  if (add) {
+    blogPostList.push(post);
+  } else {
+    let postId = post._id;
+    fetch(url + postId, {
+      method: "DELETE", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify(postId),
+    })
+      .then((res) => console.log(res))
+      // .then((res) => console.log(res))
+      .catch((data) => console.log(data));
+  }
+  renderBlogPosts();
+};
 
 const openModal = () => {
   $(".btn").click(function(){
